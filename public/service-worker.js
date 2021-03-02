e63e4d1bd354b5d5b83a24b46701f893cb7f8068
Laura Gwen <lauraburch.lb@gmail.com>
2021-03-02 00:37:33 -0500

    fixed quotes

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,11 @@ const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/styles.css',
-    `/icons/icon-192x192.png`,
-    `/icons/icon-512x512.png`,
-    `/db.js`,
-    `/index.js`,
-    `/manifest.json`
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/db.js',
+    '/index.js',
+    '/manifest.json'
 
   ];
   
